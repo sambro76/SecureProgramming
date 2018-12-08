a1e057dfaf06705eee3ffed5ec281ac254d2ad51
samnang chay <33301864+sambro76@users.noreply.github.com>
2018-12-07 20:18:37 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php 
 /*
  ============================================================================
- Name        : SP Share
+ Project name: SP Share
  Author      : Samnang Chay, Id: 2321
  Version     : 1.0.0
  Copyright   : 2018
- Description : CSE5382-Assignment 3
+ Description : Online File Sharing with Security
  ============================================================================
  */
 
