10ed3d0f39e648071a9ca2986113c8815ede9650
samnang chay <33301864+sambro76@users.noreply.github.com>
2018-12-07 20:16:41 -0600

    Update d_or_d.php

--- d_or_d.php
+++ d_or_d.php
@@ -1,14 +1,13 @@
 <?php
 /*
  ============================================================================
- Name        : SP Share
+ Project name: SP Share
  Author      : Samnang Chay, Id: 2321
  Version     : 1.0.0
  Copyright   : 2018
- Description : CSE5382-Assignment 3
+ Description : Online File Sharing with Security
  ============================================================================
  */
-session_set_cookie_params(0);
 
 session_start();
 include 'db_connect.php';
